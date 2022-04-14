031e12cefb0163d3b0cc24d9eb3cfe7cb1f31a70
Lemon Patwari <lemonpatwari@gmail.com>
2022-04-15 01:00:39 +0600

    project upload and bootstarp install

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="col-lg-8 col-md-8 col-sm-12 offset-lg-2 offset-md-2">
+            <div class="card box1">
+                <div class="card-header">
+                    Featured
+                </div>
+
+                <div class="card-header">
+                    <input type="text" class="form-control" placeholder="Enter value">
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Cras justo odio</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.box1{
+    text-align: left;
+}
+</style>
+
+<script>
+
+export default {
+    name: 'App',
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+
+
+createApp(App).mount('#app')
+
+
